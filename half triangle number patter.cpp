d87c2f169a4eb656d322a8e31e645c771edfc49b
shreya1881 <2704shreya@gmail.com>
2023-01-15 18:57:23 +0530

    .

---
+++ half triangle number patter.cpp
@@ -0,0 +1,23 @@
+/* n=3
+1
+2 2
+3 3 3
+*/
+#include <iostream>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int i=1;
+  while(i<=n){
+       int j=1;
+       while(j<=i){
+      cout<<i<<" ";
+      j++;
+    }
+    cout<<endl;
+    i++;
+   
+  }
+  return 0;
+}
