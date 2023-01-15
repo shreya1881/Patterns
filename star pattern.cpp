24741a5be22ccb5b6b80d319cb8f8e47c401770a
shreya1881 <2704shreya@gmail.com>
2023-01-15 18:06:47 +0530

    first commit

---
+++ star pattern.cpp
@@ -0,0 +1,22 @@
+// Pattern n=3
+// * * *
+// * * *
+// * * *
+
+#include <iostream>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int i=1;
+  while(i<=n){
+    int j=1;
+    while(j<=n){
+      cout<<"* ";
+      j++;
+    }
+    cout<<endl;
+    i++;
+  }
+  return 0;
+}
