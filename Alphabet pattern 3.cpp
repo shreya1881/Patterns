0ad815fc6db28068ac73f8e64627997949c20521
shreya1881 <2704shreya@gmail.com>
2023-01-16 09:33:21 +0530

    .

---
+++ Alphabet pattern 3.cpp
@@ -0,0 +1,27 @@
+/* n=3
+A B C
+D E F
+G H I
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int i=1;
+  char c = 'A';
+  while(i<=n){
+       int j=1;
+       while(j<=n){
+          
+      cout<<c<<" ";
+      c++;
+      j++;
+    }
+    cout<<endl;
+    i++;
+   
+  }
+  return 0;
+}
