bc3c8e2353d7b887f4faef30a80effa6f868648a
shreya1881 <2704shreya@gmail.com>
2023-01-16 09:50:56 +0530

    .

---
+++ half triangle alphabet pattern 3.cpp
@@ -0,0 +1,27 @@
+/* n=3 
+A
+B C
+C D E
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  int i=1;
+
+  while(i<=n){
+       int j=1;
+       while(j<=i){
+             char ch='A'+i+j-2;
+         cout<<ch<<" ";
+       
+         j++;
+    }
+    cout<<endl;
+    i++;
+   
+  }
+  return 0;
+}
